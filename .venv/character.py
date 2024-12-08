eaf54e0f372a40298cb59d447aee140393ed61fb
Krisson <sofiakytovenko35@gmail.com>
2024-12-08 14:35:13 +0200

    init

---
+++ .venv/character.py
@@ -0,0 +1,23 @@
+class Character:
+    name = ""
+    health = 100
+    damage = 5
+    defence = 0
+
+    def __init__(self, name, health=100, damage=5, defence=0):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defence = defence
+
+    def print_stats(self):
+        print(f" -< name: {self.name} >- ")
+        print(f" <- HP: {self.health} -> ")
+        print(f" <- DMG: {self.damage} -> ")
+        print(f" <- DEF: {self.defence} -> ")
+
+        def take_damage(self, damage):
+            self.health = max(self.health - damage, 0)
+
+        def attack(self, target):
+                target.take_damage(self.damage)
